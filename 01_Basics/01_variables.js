f61c8747a8bff7611f77426bbe3ddb81442ab6bc
pawaramar763 <pawaramar763@gmail.com>
2024-01-14 20:39:38 +0530

    data types and variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1452356;
+let accountEmail = 'abc@gmail.com';
+var  accountPassword = "987456";
+accountCity = "Pune";
+
+// accountId = 2546354;      //not allowed for const type;
+accountEmail = 'xyg@gmail.com';
+accountPassword = '45126';
+accountCity = "Mumbai";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer  version
+
+let name ="JavaScript";
+let age =90;
+let isLoggedIn = true;
+
+//number
+//bigint
+//string => "" && ''
+//boolean => true/false
+//null => stand alone value
+//undefined
+//symbol
+console.log(typeof(age)); //number
+console.log(typeof "hello");  //string
+
+console.log(typeof null);   // object
+console.log(typeof undefined);  //undefined
+
+
+
+//object
